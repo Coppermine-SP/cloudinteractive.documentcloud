dff032ef97c3294e32c2650f7015f70f05e001b6
Coppermine-SP <son5121502@gmail.com>
2024-01-02 03:46:28 +0900

    feat: redis 서버를 통한 request 상태 관리

--- RequestManagement.cs
+++ RequestManagement.cs
@@ -1,26 +1,68 @@
-﻿using System.Runtime.InteropServices.JavaScript;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Http.Metadata;
+using StackExchange.Redis;
+using Microsoft.Extensions.Configuration;
 
 namespace cloudinteractive.documentcloud
 {
     public static class RequestManagement
     {
-        private static ILogger? logger;
-        public static void Init()
+        private static ILogger? _logger;
+        private static ConnectionMultiplexer _redis;
+        private static IDatabase _db;
+        private static IConfiguration _config;
+        public static void Init(IConfiguration config)
         {
-            logger = ApplicationLogging.CreateLogger("RequestManagement");
-            logger.LogInformation("RequestManagement init..");
+            _logger = ApplicationLogging.CreateLogger("RequestManagement");
+            _logger.LogInformation("RequestManagement init..");
+            _config = config;
+
+            string server = config.GetSection("RedisServer").Value ?? "127.0.0.1:6379";
+            _logger.LogInformation($"Connect to Redis server : {server}...");
+            _redis = ConnectionMultiplexer.Connect(new ConfigurationOptions
+            {
+                EndPoints = { server }
+            });
+            _db = _redis.GetDatabase();
+
+            var pong = _db.Ping();
+            _logger.LogInformation($"Successfully connected. (took {pong}sec.)");
+
         }
 
-        public enum RequestStatus {Queued, Exporting, Processing, Complete, Error}
+        public enum RequestStatus {Queued = 0, Exporting = 1, Processing = 2, Complete = 3, Error = 4}
 
-        public static void ChangeRequestStatus(string requestId, RequestStatus status)
+        private static string _getStatusKey(string requestId)
         {
+            return $"{requestId}:status";
+        }
 
+        private static string _getResultKey(string requestId)
+        {
+            return $"{requestId}:result";
+        }
+
+        public static void SetRequestStatus(string requestId, RequestStatus status)
+        {
+            _db.StringSet(_getStatusKey(requestId), ((int)status).ToString(), TimeSpan.FromMinutes(10));
         }
 
         public static void SetRequestResult(string requestId, string result)
         {
+            _db.StringSet(_getResultKey(requestId), result, TimeSpan.FromMinutes(10));
+        }
 
+        public static RequestStatus? GetRequestStatus(string requestId)
+        {
+            string? status = _db.StringGet(_getStatusKey(requestId));
+
+            if (status is null) return null;
+            return (RequestStatus)Convert.ToInt32(status);
+        }
+
+        public static string? GetRequestResult(string requestId)
+        {
+            return _db.StringGet(_getResultKey(requestId));
         }
     }
 }
